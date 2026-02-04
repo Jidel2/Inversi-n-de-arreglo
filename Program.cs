8b6dde4626f61e91e4ace2e933c57ca1ce4516b2
Jidel2 <amoputo110@gmail.com>
2026-02-03 22:38:28 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class InvertirArreglo
+{
+    static void Main()
+    {
+        int[] numeros = new int[6];
+
+        Console.WriteLine("Ingrese 6 números:");
+
+        for (int i = 0; i < 6; i++)
+        {
+            Console.Write($"Número {i + 1}: ");
+            numeros[i] = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("\nArreglo en orden inverso:");
+
+        for (int i = 5; i >= 0; i--)
+        {
+            Console.Write(numeros[i] + " ");
+        }
+    }
+}
